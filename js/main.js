02cf15650da30d96c9d3b5f0ac1071a789ea85e4
fahimsheikh18 <fahimbolod1@gmail.com>
2025-12-04 03:05:11 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// bx slider
+
+
+$(document).ready(function(){
+  $('.slider-area').bxSlider();
+  $('#main-menu').meanmenu(
+    {
+      meanmenucontainer:'#mobile-menu',
+      meanScreenWidth:991,
+    }
+  );
+});
+
+
